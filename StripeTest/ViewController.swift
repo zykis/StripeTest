abc1410cebd7ef4848979769a444c7a598e81618
Artem <zykis39@gmail.com>
2019-11-19 15:35:30 +0300

    Initial Commit

---
+++ StripeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StripeTest
+//
+//  Created by Артём Зайцев on 19.11.2019.
+//  Copyright © 2019 Артём Зайцев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
